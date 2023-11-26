bd3a0614fbfed90f65d15c1bbb1e28d70d200640
xiaojunnanya <xjntofly@sina.com>
2023-11-26 17:05:21 +0800

    完善自定义提取码

--- src/components/Table/Handle/Share/index.tsx
+++ src/components/Table/Handle/Share/index.tsx
@@ -1,8 +1,8 @@
-import { Button, Form, Modal, Radio, message } from 'antd'
+import { Button, Form, Modal, Radio, message, Input } from 'antd'
 import React, { forwardRef, memo, useImperativeHandle, useRef, useState } from 'react'
 import { DataType } from '../../type';
 import { ChildShareMethods } from '../..';
-import { shareFile } from '@/service/modules/share';
+import { shareFile, shareFileUrl } from '@/service/modules/share';
 import { coppyUrl } from '@/utils';
 
 const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
@@ -14,8 +14,6 @@ const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
     }
   }, []);
 
-  const ref1 = useRef()
-  const ref2 = useRef()
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [ recordData, setRecordData ] = useState<DataType>()
@@ -26,8 +24,8 @@ const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
       shareId: '',
       code: ''
     })
-
     const [ cancelText, setCancelText ] = useState('取消')
+    const [ radio2Value, setRadio2Value ] = useState('1')
 
     const openModel = (record: DataType) =>{
       setShareUrl({
@@ -40,16 +38,24 @@ const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
     }
 
     const handleOk = async () => {
-      const res = await form.validateFields()
-      console.log(res);
-      const res1 = await shareFile(recordData!.fileId, res.radio1, res.radio2)
-      console.log(res1.data.data.code); // 提取码: code
-      console.log(res1.data.data.shareId); // 分享连接:http://netdisk.kbws.xyz/share/shareId
-      setShareUrl({
-        shareId: res1.data.data.shareId,
-        code: res1.data.data.code
-      })
-      setCancelText('关闭')
+      try {
+        const res = await form.validateFields()
+        console.log(res);
+        const res1 = await shareFile(recordData!.fileId, res.radio1, res.radio2, res.code && res.code)
+        setShareUrl({
+          shareId: res1.data.data.shareId,
+          code: res1.data.data.code
+        })
+        setCancelText('关闭')
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+
+    const radio2Change = (e: any) =>{
+      console.log(e.target.value);
+      setRadio2Value(e.target.value)
     }
 
   return (
@@ -78,7 +84,7 @@ const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
               ( shareUrl.shareId && shareUrl.code ) ? (
                 <>
                   <Form.Item label="分享连接">
-                    { shareUrl.shareId }
+                    { shareFileUrl(shareUrl.shareId) }
                   </Form.Item>
                   <Form.Item label="提取码">
                     { shareUrl.code }
@@ -92,20 +98,43 @@ const index = memo(forwardRef<ChildShareMethods>((props, ref) => {
               ) : (
                 <>
                   <Form.Item label="有效期" name='radio1' initialValue={'0'}>
-                  <Radio.Group>
-                    <Radio value="0"> 1天 </Radio>
-                    <Radio value="1"> 7天 </Radio>
-                    <Radio value="2"> 30天 </Radio>
-                    <Radio value="3"> 永久有效 </Radio>
-                  </Radio.Group>
-                </Form.Item>
+                    <Radio.Group>
+                      <Radio value="0"> 1天 </Radio>
+                      <Radio value="1"> 7天 </Radio>
+                      <Radio value="2"> 30天 </Radio>
+                      <Radio value="3"> 永久有效 </Radio>
+                    </Radio.Group>
+                  </Form.Item>
 
-                <Form.Item label="提取码" name='radio2' initialValue={'1'}>
-                  <Radio.Group>
-                    <Radio value="1"> 系统生成 </Radio>
-                    <Radio value="0"> 自定义 </Radio>
-                  </Radio.Group>
-                </Form.Item>
+                  <Form.Item label="提取码" name='radio2' initialValue={'1'}>
+                    <Radio.Group onChange={radio2Change}>
+                      <Radio value="1"> 系统生成 </Radio>
+                      <Radio value="0"> 自定义 </Radio>
+                    </Radio.Group>
+                  </Form.Item>
+
+                  {
+                    radio2Value === '0' && (
+                      <>
+                        <Form.Item label="提取码" name='code' rules={
+                          [
+                            { 
+                              required: true, 
+                              message:"请输入提取码" 
+                            }, 
+                            { 
+                              type: 'string', 
+                              len: 5,
+                              message:"提取码必须为5位"
+                            }
+                          ]
+                        }>
+                          <Input showCount maxLength={5} placeholder='请输入5位提取码'/>
+                        </Form.Item>
+                      </>
+                    )
+                  }
+
                 </>
               )
             }
